cbef428027c24a7279ff1fab8753baae8f8a939f
Ran Cohen <rannn505@outlook.com>
2022-10-06 12:47:48 +0300

    bump @configu/ts

--- ts/packages/cli/src/commands/init.ts
+++ ts/packages/cli/src/commands/init.ts
@@ -4,14 +4,14 @@ import path from 'path';
 import { cwd } from 'process';
 import _ from 'lodash';
 import { paramCase } from 'change-case';
-import { CFGU, CfguType, CfguContents } from '@configu/ts';
+import { CfguType, CfguContents } from '@configu/ts';
 import { extractConfigs } from '@configu/lib';
 import { Cfgu } from '@configu/node';
 import { BaseCommand } from '../base';
 import { getPathBasename } from '../helpers/utils';
 
 export default class Init extends BaseCommand {
-  static description = `creates a config schema ${CFGU.EXT} file in the current working dir`;
+  static description = `creates a config schema ${Cfgu.EXT} file in the current working dir`;
   static examples = [
     '<%= config.bin %> <%= command.id %> --name "cli"',
     '<%= config.bin %> <%= command.id %> --dir "./src/cli" --name "cli"',
@@ -22,39 +22,36 @@ export default class Init extends BaseCommand {
   static flags = {
     ...BaseCommand.flags,
     dir: Flags.string({
-      description: `overrides the directory that will contain the new ${CFGU.EXT} file`,
+      description: `overrides the directory that will contain the new ${Cfgu.EXT} file`,
       default: cwd(),
     }),
     name: Flags.string({
-      description: `overrides the name for the new ${CFGU.EXT} file`,
+      description: `overrides the name for the new ${Cfgu.EXT} file`,
       default: paramCase(getPathBasename()),
     }),
     force: Flags.boolean({
       char: 'f',
-      description: `overrides the ${CFGU.EXT} file in case it already exists`,
+      description: `overrides the ${Cfgu.EXT} file in case it already exists`,
       default: false,
     }),
 
     examples: Flags.boolean({
       exclusive: ['import'],
-      description: `fills the new ${CFGU.EXT} file with a variety of detailed examples`,
+      description: `fills the new ${Cfgu.EXT} file with a variety of detailed examples`,
       default: false,
     }),
 
     import: Flags.string({
       exclusive: ['examples'],
-      description:
-        'use this flag to import an existing .env file and create a cfgu file from it. Then push the newly created cfgu to create a Configu schema',
+      description: `use this flag to import an existing .env file and create a ${Cfgu.EXT} file from it. Then push the newly created ${Cfgu.NAME} to create a Configu schema`,
     }),
     defaults: Flags.boolean({
       dependsOn: ['import'],
-      description:
-        'use this flag to assign the values from your .env file as the default value for the keys that will be created in the cfgu file.',
+      description: `use this flag to assign the values from your .env file as the default value for the keys that will be created in the ${Cfgu.EXT} file.`,
     }),
     types: Flags.boolean({
       dependsOn: ['import'],
-      description:
-        'use this flag, so that Configu will infer the types of your keys from their values, and create the cfgu with those types. Otherwise all keys are created with the String type.',
+      description: `use this flag, so that Configu will infer the types of your keys from their values, and create the ${Cfgu.NAME} with those types. Otherwise all keys are created with the String type.`,
     }),
   };
 
@@ -63,14 +60,14 @@ export default class Init extends BaseCommand {
 
     const hasOverrideName = flags.name !== getPathBasename();
     const fileName = hasOverrideName ? flags.name : getPathBasename(flags.dir);
-    const fileNameWithExt = `${fileName}${CFGU.EXT}.${CfguType.Json}`;
+    const fileNameWithExt = `${fileName}${Cfgu.EXT}.${CfguType.Json}`;
     const filePath = path.resolve(flags.dir, fileNameWithExt);
     const cfgu = new Cfgu(filePath);
 
     let fileContentData: CfguContents = {};
 
     if (flags.examples) {
-      fileContentData = CFGU.EXAMPLE;
+      fileContentData = Cfgu.EXAMPLE;
     }
 
     if (flags.import) {
@@ -86,7 +83,7 @@ export default class Init extends BaseCommand {
     const fileContent = JSON.stringify(fileContentData, null, 2);
 
     await fs.writeFile(filePath, fileContent, { flag: flags.force ? 'w' : 'wx' }); // * https://nodejs.org/api/fs.html#file-system-flags
-    fileContentData = await cfgu.parse();
+    fileContentData = await Cfgu.parse(cfgu);
 
     const recordsCount = _.keys(fileContentData).length;
     this.log(`${filePath} generated with ${recordsCount} records`);
