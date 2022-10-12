156fe4eda66bbd3561b8b25efacfc1934f34b413
Richard Akman <richard@configu.com>
2022-10-12 10:01:41 +0300

    Add validate function to JTD (#5)

--- ts/packages/ts/src/types/index.ts
+++ ts/packages/ts/src/types/index.ts
@@ -1,7 +1,9 @@
+import CfguJTDSchema from './generated/Cfgu.jtd.json';
 import CfguContentsJTDSchema from './generated/CfguContents.jtd.json';
+import StoreJTDSchema from './generated/Store.jtd.json';
 import StoreContentsJTDSchema from './generated/StoreContents.jtd.json';
 
-export { CfguContentsJTDSchema, StoreContentsJTDSchema };
+export { CfguJTDSchema, CfguContentsJTDSchema, StoreContentsJTDSchema, StoreJTDSchema };
 
 export { CfguType, Cfgu as ICfgu } from './generated/Cfgu';
 export { ConfigSchemaType, ConfigSchema, CfguContents } from './generated/CfguContents';
