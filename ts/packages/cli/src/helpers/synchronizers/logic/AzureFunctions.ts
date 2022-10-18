b50363a78c512fbdcf898e9d4060e564c4b77eb0
Richard <richard.akman1@gmail.com>
2022-10-18 15:44:17 +0300

    chore: adjust azure runtime comments

--- ts/packages/cli/src/helpers/synchronizers/logic/AzureFunctions.ts
+++ ts/packages/cli/src/helpers/synchronizers/logic/AzureFunctions.ts
@@ -6,7 +6,7 @@ import { CONFIG_SYNCHRONIZER_LABEL } from '@configu/lib';
 const label = CONFIG_SYNCHRONIZER_LABEL.AzureFunctions;
 
 export type AzureFunctionsConfiguration = {
-  // * Azure functions docs: https://learn.microsoft.com/en-us/azure/app-service/configure-common
+  // * Azure functions docs: https://learn.microsoft.com/en-us/azure/app-service/configure-common#configure-app-settings
   tenantId: string;
   clientId: string;
   clientSecret: string;
@@ -15,7 +15,6 @@ export type AzureFunctionsConfiguration = {
   functionApp: string;
 };
 
-// TODO - Decide between syncAzureFunctionsConfigs (previous name) and syncAzureFunctionsAppSettings
 export const syncAzureFunctionsAppSettings = async ({
   configuration,
   configs,
