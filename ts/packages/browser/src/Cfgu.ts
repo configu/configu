04ad4a5ac403d2518cc1be69b9d55249907e712a
Ran Cohen <rannn505@outlook.com>
2022-10-29 20:24:04 -0400

    Update Cfgu.ts

--- ts/packages/browser/src/Cfgu.ts
+++ ts/packages/browser/src/Cfgu.ts
@@ -1,6 +1,14 @@
 import { Cfgu as BaseCfgu } from '@configu/ts';
 import { fileOpen } from 'browser-fs-access';
 
+const FILE_OPEN_DEFAULT_OPTIONS = {
+  description: `${BaseCfgu.NAME} files`,
+  // todo: submit an application for a cfgu vendor Media Type - https://www.iana.org/assignments/media-types/media-types.xhtml#text
+  // mimeTypes: [`application/${CfguType.Json}`],
+  // extensions: Cfgu.TYPES.map((type) => `${Cfgu.EXT}.${type}`),
+  extensions: BaseCfgu.TYPES.map((type) => `.${type}`),
+};
+
 export class Cfgu extends BaseCfgu {
   constructor(public readonly blob: File) {
     super(blob.name);
@@ -10,20 +18,13 @@ export class Cfgu extends BaseCfgu {
     this.contents = await this.blob.text();
   }
 
-  static async init(multiple: false): Promise<Cfgu>;
-  static async init(multiple: true): Promise<Cfgu[]>;
-  static async init(multiple = false): Promise<Cfgu | Cfgu[]> {
-    const blob = await fileOpen({
-      description: 'Cfgu files',
-      // todo: submit an application for a cfgu vendor Media Type - https://www.iana.org/assignments/media-types/media-types.xhtml#text
-      // mimeTypes: [`application/${CfguType.Json}`],
-      // extensions: Cfgu.TYPES.map((type) => `${Cfgu.EXT}.${type}`),
-      extensions: Cfgu.TYPES.map((type) => `.${type}`),
-      multiple,
-    });
-    if (Array.isArray(blob)) {
-      return blob.map((b) => new Cfgu(b));
-    }
+  static async init(): Promise<Cfgu> {
+    const blob = await fileOpen({ ...FILE_OPEN_DEFAULT_OPTIONS });
     return new Cfgu(blob);
+  }
+
+  static async initBulk(): Promise<Cfgu[]> {
+    const blobs = await fileOpen({ ...FILE_OPEN_DEFAULT_OPTIONS, multiple: true });
+    return blobs.map((b) => new Cfgu(b));
   }
 }
