5eff7fb99644d23b27d47663854ac9503fbd5952
Ran Cohen <ran@configu.com>
2022-11-09 14:28:59 +0200

    Rename cli store command flag to --uri (#27)
    Update store.ts

--- ts/packages/cli/src/commands/store.ts
+++ ts/packages/cli/src/commands/store.ts
@@ -8,19 +8,19 @@ export default class Store extends BaseCommand {
   static description = 'caches config store credentials as names to later be used as --store flag value';
 
   static examples = [
-    '<%= config.bin %> <%= command.id %> --name "default" --url "configu://-"',
-    '<%= config.bin %> <%= command.id %> --name "secrets" --url "hashicorp-vault://token@address"',
+    '<%= config.bin %> <%= command.id %> --name "default" --uri "configu://-"',
+    '<%= config.bin %> <%= command.id %> --name "secrets" --uri "hashicorp-vault://token@address"',
   ];
 
   static flags = {
     name: Flags.string({ required: true, description: 'name to assign to the cached store' }),
-    url: Flags.string({ required: true, description: 'store url to cache' }),
+    uri: Flags.string({ required: true, description: 'store uri to cache' }),
   };
 
   public async run(): Promise<void> {
     const { flags } = await this.parse(Store);
 
-    const { url } = await constructStoreFromUrl(flags.url);
+    const { url } = await constructStoreFromUrl(flags.uri);
 
     const configDataWithNewStore = _.set(this.config.configData, `stores.${flags.name}`, url);
     const rawConfigData = JSON.stringify(configDataWithNewStore);
