0945a83eb6ee82960a140081e27816ffdfc09afc
Richard <richard.akman1@gmail.com>
2022-11-17 11:58:22 +0200

    refactor: remove k8s redundant validation

--- ts/packages/cli/src/helpers/stores/Kubernetes.ts
+++ ts/packages/cli/src/helpers/stores/Kubernetes.ts
@@ -6,10 +6,6 @@ export const KubernetesSecretStoreSTI: SchemeToInit = {
     const kubeconfigFilePath = `${parsedUri.host}${parsedUri.path}`;
     const namespace = queryDict.namespace ?? 'default';
 
-    if (!kubeconfigFilePath) {
-      throw new Error(`invalid store uri ${uri}`);
-    }
-
     // * kubernetes-secret://path/to/kubeconfig.yaml[?namespace=]
     return {
       uri,
