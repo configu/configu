a27b4a54e7e533ae77f622e588f7db93d148ac3e
Richard <richard.akman1@gmail.com>
2022-11-17 21:55:20 +0200

    fix(cli): k8s store protocol ref to scheme

--- ts/packages/cli/src/helpers/stores/Kubernetes.ts
+++ ts/packages/cli/src/helpers/stores/Kubernetes.ts
@@ -2,7 +2,7 @@ import { KubernetesSecretStore } from '@configu/node';
 import { SchemeToInit } from './types';
 
 export const KubernetesSecretStoreSTI: SchemeToInit = {
-  [KubernetesSecretStore.protocol]: async ({ uri, parsedUri, queryDict }) => {
+  [KubernetesSecretStore.scheme]: async ({ uri, parsedUri, queryDict }) => {
     const kubeconfigFilePath = `${parsedUri.host}${parsedUri.path}`;
     const namespace = queryDict.namespace ?? 'default';
 
