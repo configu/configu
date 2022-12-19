be5853d60485315d5632ac685058770c42f73dcc
Ran Cohen <rannn505@outlook.com>
2022-12-19 19:27:37 +0200

    Create LocalForage.ts

---
+++ ts/packages/browser/src/stores/LocalForage.ts
@@ -0,0 +1,29 @@
+import localForage from 'localforage';
+import { KeyValueStore } from '@configu/ts';
+
+export class LocalForageStore extends KeyValueStore {
+  private client: LocalForage;
+  constructor(configuration: LocalForageOptions) {
+    super('localforage');
+    this.client = localForage.createInstance(configuration);
+  }
+
+  // * https://localforage.github.io/localForage/#data-api-getitem
+  async getByKey(key: string): Promise<string> {
+    const data = await this.client.getItem(key);
+    if (typeof data === 'string') {
+      return data ?? '';
+    }
+    return JSON.stringify(data) ?? '';
+  }
+
+  // * https://localforage.github.io/localForage/#data-api-setitem
+  async upsert(key: string, value: string): Promise<void> {
+    await this.client.setItem(key, value);
+  }
+
+  // * https://localforage.github.io/localForage/#data-api-removeitem
+  async delete(key: string): Promise<void> {
+    await this.client.removeItem(key);
+  }
+}
