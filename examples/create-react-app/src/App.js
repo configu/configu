76d8d3afc98a308e3f1c6b36c52988c0d6d99047
Ran Cohen <rannn505@outlook.com>
2023-01-05 19:46:05 +0200

    add cfgu

--- examples/create-react-app/src/App.js
+++ examples/create-react-app/src/App.js
@@ -1,21 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header
+        className="App-header"
+        style={{backgroundColor: process.env.REACT_APP_BG_COLOR}}
+      >
+        <img
+          className="App-logo" alt="logo"
+          src={process.env.REACT_APP_LOGO_SRC}
+        />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {process.env.REACT_APP_MAIN_TEXT}
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href={process.env.REACT_APP_LINK_HREF}
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {process.env.REACT_APP_LINK_TEXT}
         </a>
       </header>
     </div>
