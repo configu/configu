d2ef028c290d1f5d303ef99d099561f11b386ebd
RonConfigu <129258356+RonConfigu@users.noreply.github.com>
2023-06-04 13:21:02 +0300

    TestCommand for ConfigStore (#110)

---
+++ ts/packages/cli/src/commands/test.ts
@@ -0,0 +1,33 @@
+import { Flags } from '@oclif/core';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import { TestCommand } from '@configu/ts';
+import { BaseCommand } from '../base';
+
+export default class Test extends BaseCommand<typeof Test> {
+  static description = 'verify credentials and write access to a store';
+
+  static examples = ['<%= config.bin %> <%= command.id %> --store "configu" --clean'];
+
+  static flags = {
+    store: Flags.string({
+      description: 'config-store to test',
+      required: true,
+    }),
+    clean: Flags.boolean({
+      description: 'delete CONFIGU_TEST key from the store',
+      default: false,
+    }),
+  };
+
+  public async run(): Promise<void> {
+    const store = await this.getStoreInstanceByStoreFlag(this.flags.store);
+    const { clean } = this.flags;
+    try {
+      await new TestCommand({ store, clean }).run();
+      this.log(`store ${store.type} test passed`);
+    } catch (error) {
+      this.error(`store ${store.type} test failed with error: ${error.message}`);
+    }
+  }
+}
