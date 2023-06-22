26f7f7fc88fd51c102b0ee1c2aa824125883c073
lucas <lucas.james@veygo.com>
2023-06-22 16:57:32 +0100

    Write docstrings

--- py/configu/core/config_store.py
+++ py/configu/core/config_store.py
@@ -5,13 +5,31 @@ from .generated import ConfigStore as IConfigStore, ConfigStoreQuery, Config
 
 
 class ConfigStore(ABC, IConfigStore):
+    """
+    An abstract base class representing a config store.
+
+    A ConfigStore is a storage system for Configs in Configu,
+    serving as a mechanism for storing, retrieving, and managing configuration
+    data.
+    """
+
     def init(self):
-        """Anything that needs initialization before running Commands"""
+        """Creates/prepares a new ConfigStore"""
 
     @abstractmethod
     def get(self, queries: List[ConfigStoreQuery]) -> List[Config]:
+        """
+        Retrieves a set of `Config`s.
+        :param queries: a class containing either the key and/or set to be
+        retrieved.
+        :return: A list of `Config`s from the store.
+        """
         pass
 
     @abstractmethod
     def set(self, configs: List[Config]) -> None:
+        """
+        Upserts a set of `Config`s.
+        :param configs: a list of `Config`s to be set in the store.
+        """
         pass
