38301c9fd677e9f8beb2b0c23ce67c79662ceede
jamesL92 <jamesL92@users.noreply.github.com>
2023-06-26 07:35:28 +0100

    Feat/py secrets manager store (#126)

--- py/configu/stores/__init__.py
+++ py/configu/stores/__init__.py
@@ -1,5 +1,11 @@
+from .aws_secrets_manager_store import AWSSecretsManagerConfigStore
 from .configu_store import ConfiguConfigStore
 from .in_memory_store import InMemoryConfigStore
 from .json_file_store import JsonFileConfigStore
 
-__all_ = ["InMemoryConfigStore", "ConfiguConfigStore", "JsonFileConfigStore"]
+__all_ = [
+    "AWSSecretsManagerConfigStore",
+    "InMemoryConfigStore",
+    "ConfiguConfigStore",
+    "JsonFileConfigStore",
+]
