08ab688eeb2077dd0f0545babcb74c16dae3a4f0
lucas <lucas.james@veygo.com>
2023-07-04 22:35:40 +0100

    Fix js-sdk example
    - Bump @configu/node dependency version
    - Fix naming of JsonFileConfigStore

--- examples/js-sdk/index-sdk.js
+++ examples/js-sdk/index-sdk.js
@@ -2,9 +2,9 @@
   // require('dotenv').config();
 
   const path = require('path');
-  const { JsonFileStore, ConfiguStore, ConfigSet, ConfigSchema, ExportCommand } = require('@configu/node')
+  const { JsonFileConfigStore, ConfigSet, ConfigSchema, ExportCommand } = require('@configu/node')
 
-  const store = new JsonFileStore(path.resolve(__dirname, './store.json'));
+  const store = new JsonFileConfigStore(path.resolve(__dirname, './store.json'));
   const set = new ConfigSet(process.env.NODE_ENV);
   const schema = new ConfigSchema(path.resolve(__dirname, './get-started.cfgu.json'));
 
