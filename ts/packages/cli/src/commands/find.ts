912c3e58334c163183a56f16e19fca27e29b3d63
Shriekin’ Ninja <ninja@l-m-group.com>
2023-07-06 16:24:53 +0300

    cr comments

--- ts/packages/cli/src/commands/find.ts
+++ ts/packages/cli/src/commands/find.ts
@@ -1,4 +1,4 @@
-import { Args, Flags } from '@oclif/core';
+import { Flags } from '@oclif/core';
 import path from 'path';
 import { ConfigSchema } from '@configu/node';
 import { createInterface } from 'readline';
@@ -10,8 +10,6 @@ import FastGlob = require('fast-glob');
 import { BaseCommand } from '../base';
 
 export default class Find extends BaseCommand<typeof Find> {
-  private ignoredFiles: Set<string> = new Set(['build', 'dist', 'node_modules']);
-
   static description = `Finding all config parameters and their usage in the source code based on CFGU files`;
 
   static examples = [
@@ -29,55 +27,50 @@ export default class Find extends BaseCommand<typeof Find> {
     },
   ];
 
-  static args = {
-    path: Args.directory({
+  static flags = {
+    dir: Flags.directory({
       description: 'Path to source code',
       required: true,
       default: '.',
     }),
-  };
-
-  static flags = {
-    cfgu: Flags.file({
+    include: Flags.file({
       description: 'Path to a specific .cfgu file (default: all CFGU files in the source code)',
       required: false,
+      multiple: true,
+      default: [],
+    }),
+    exclude: Flags.file({
+      description: 'Glob pattern to ignore',
+      required: false,
+      multiple: true,
+      default: [],
     }),
     unused: Flags.boolean({
       description: 'Only show unused parameters',
       required: false,
       default: false,
     }),
-    'show-templates': Flags.boolean({
+    templates: Flags.boolean({
       description: 'Ignore parameters that are parts of templates and treat them as used parameters',
       required: false,
       default: false,
-    }),
-    ignore: Flags.string({
-      description: 'Comma separated list of glob patterns to ignore. (ex. --ignore "*.xml,**/*.md")',
-      required: false,
-      default: '',
+      allowNo: true,
     }),
   };
 
   private async updateIgnoredFiles(projectPath: string) {
-    this.ignoredFiles = new Set([...this.ignoredFiles, ...this.flags.ignore.split(',')]);
+    const ignoredFiles = new Set(['build', 'dist', 'node_modules', ...this.flags.exclude]);
     const gitIgnores = FastGlob.sync([path.join(projectPath, '.gitignore')], {
       dot: true,
-      ignore: [...this.ignoredFiles].map((dirName) => path.join(projectPath, `/**/${dirName}`)),
+      ignore: [...ignoredFiles].map((dirName) => path.join(projectPath, `/**/${dirName}`)),
     });
-    await Promise.all(
-      gitIgnores.map((gitIgnore) =>
-        this.processLineByLine(gitIgnore, (line, lineIndex) => {
-          if (!line.startsWith('#') && !this.ignoredFiles.has(line)) this.ignoredFiles.add(line);
-        }),
-      ),
+    const gitIgnorePromises = gitIgnores.map((gitIgnore) =>
+      this.processLineByLine(gitIgnore, (line, lineIndex) => {
+        if (!line.startsWith('#') && !ignoredFiles.has(line)) ignoredFiles.add(line);
+      }),
     );
-  }
-
-  private findCfgu(projectPath: string) {
-    return FastGlob.sync([path.join(projectPath, '/**/*.cfgu.json')], {
-      ignore: [...this.ignoredFiles].map((ignoreFile) => path.join(projectPath, `/**/${ignoreFile}`)),
-    });
+    await Promise.all(gitIgnorePromises);
+    return ignoredFiles;
   }
 
   private async processLineByLine(filePath: string, onLine: (line: string, lineIndex: number) => any): Promise<any[]> {
@@ -96,12 +89,17 @@ export default class Find extends BaseCommand<typeof Find> {
     return result;
   }
 
-  private async find() {
-    const projectPath = path.resolve(this.args.path);
-    const unusedOnly = this.flags.unused;
-    const showTemplateKeys = this.flags['show-templates'];
-    await this.updateIgnoredFiles(projectPath);
-    const cfguPaths = this.flags.cfgu ? [this.flags.cfgu] : this.findCfgu(projectPath);
+  public async run(): Promise<void> {
+    const findInDirectory = path.resolve(this.flags.dir);
+    const showUnusedOnly = this.flags.unused;
+    const showTemplateKeys = this.flags.templates;
+    const ignoredFiles = await this.updateIgnoredFiles(findInDirectory);
+    const cfguPaths =
+      this.flags.include.length > 0
+        ? this.flags.include
+        : FastGlob.sync([path.join(findInDirectory, '/**/*.cfgu.json')], {
+            ignore: [...ignoredFiles].map((ignoreFile) => path.join(findInDirectory, `/**/${ignoreFile}`)),
+          });
 
     if (cfguPaths.length === 0)
       throw new Error(
@@ -137,9 +135,9 @@ export default class Find extends BaseCommand<typeof Find> {
       count: 0,
     }));
 
-    const files = FastGlob.sync([path.join(projectPath, '/**/*.*')], {
+    const files = FastGlob.sync([path.join(findInDirectory, '/**/*.*')], {
       dot: true,
-      ignore: [...[...this.ignoredFiles].map((dirName) => path.join(projectPath, `/**/${dirName}`)), '/**/*.json'],
+      ignore: [...[...ignoredFiles].map((dirName) => path.join(findInDirectory, `/**/${dirName}`)), '/**/*.json'],
     });
 
     await Promise.all(
@@ -152,8 +150,8 @@ export default class Find extends BaseCommand<typeof Find> {
               const match = regEx.exec(line);
               if (line && match) {
                 keyRegEx.count += 1;
-                if (!unusedOnly)
-                  this.log(`${file.replace(projectPath, '...')}:${lineIndex}:${match.index} [${regEx.source}]`);
+                if (!showUnusedOnly)
+                  this.log(`${file.replace(findInDirectory, '...')}:${lineIndex}:${match.index} [${regEx.source}]`);
               }
             }
           });
@@ -162,14 +160,6 @@ export default class Find extends BaseCommand<typeof Find> {
     );
     const unusedKeys = Object.keys(keysRegEx).filter((key) => keysRegEx[key]?.count === 0);
     if (unusedKeys.length > 0) this.log(`Unused configs found: ${unusedKeys.join(', ')}`);
-    else if (unusedOnly) this.log('No unused configs found');
-  }
-
-  public async run(): Promise<void> {
-    try {
-      await this.find();
-    } catch (error) {
-      this.log(`ERROR: ${error.message}`);
-    }
+    else if (showUnusedOnly) this.log('No unused configs found');
   }
 }
