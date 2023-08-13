27c553ce49e7aa690482f1c5dd40555a2722d781
Sadaf-A <sadaf20043006@gmail.com>
2023-08-13 17:50:40 +0530

    feat(DateTime): adding DateType type to configu

--- ts/packages/ts/src/types/generated.ts
+++ ts/packages/ts/src/types/generated.ts
@@ -32,7 +32,7 @@ export interface Cfgu {
     type:         CfguType;
 }
 
-export type CfguType = "Base64" | "Boolean" | "Color" | "ConnectionString" | "Country" | "Currency" | "Domain" | "Email" | "Hex" | "IPv4" | "IPv6" | "LatLong" | "Locale" | "MD5" | "MobilePhone" | "Number" | "RegEx" | "SHA" | "SemVer" | "String" | "URL" | "UUID" | "DockerImage";
+export type CfguType = "Base64" | "Boolean" | "Color" | "ConnectionString" | "Country" | "Currency" | "Domain" | "Email" | "Hex" | "IPv4" | "IPv6" | "LatLong" | "Locale" | "MD5" | "MobilePhone" | "Number" | "RegEx" | "SHA" | "SemVer" | "String" | "URL" | "UUID" | "DockerImage" | "DateTime";
 
 /**
  * A generic representation of a software configuration, aka Config
@@ -395,6 +395,7 @@ const typeMap: any = {
         "ConnectionString",
         "Country",
         "Currency",
+        "DateTime",
         "DockerImage",
         "Domain",
         "Email",
