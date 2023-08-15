4b3c167840be3a25e75afb10edb5340a4230e9a4
Sadaf-A <sadaf20043006@gmail.com>
2023-08-15 22:01:45 +0530

    feat(MIMEType): adding MIMEType Cfgu Type

--- ts/packages/ts/src/types/generated.ts
+++ ts/packages/ts/src/types/generated.ts
@@ -32,7 +32,7 @@ export interface Cfgu {
     type:         CfguType;
 }
 
-export type CfguType = "Base64" | "Boolean" | "Color" | "ConnectionString" | "Country" | "Currency" | "Domain" | "Email" | "Hex" | "IPv4" | "IPv6" | "LatLong" | "Locale" | "MD5" | "MobilePhone" | "Number" | "RegEx" | "SHA" | "SemVer" | "String" | "URL" | "UUID" | "DockerImage";
+export type CfguType = "Base64" | "Boolean" | "Color" | "ConnectionString" | "Country" | "Currency" | "Domain" | "Email" | "Hex" | "IPv4" | "IPv6" | "LatLong" | "Locale" | "MD5" | "MobilePhone" | "Number" | "RegEx" | "SHA" | "SemVer" | "String" | "URL" | "UUID" | "DockerImage" | "MIMEType";
 
 /**
  * A generic representation of a software configuration, aka Config
@@ -404,6 +404,7 @@ const typeMap: any = {
         "LatLong",
         "Locale",
         "MD5",
+        "MIMEType",
         "MobilePhone",
         "Number",
         "RegEx",
