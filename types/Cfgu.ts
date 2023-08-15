01ac76b1dc5eb53802d3ef5e1c10f8a3682339e7
Sadaf-A <sadaf20043006@gmail.com>
2023-08-15 22:07:26 +0530

    feat(CreditCard): adding CreditCard Cfgu Type

--- types/Cfgu.ts
+++ types/Cfgu.ts
@@ -21,7 +21,8 @@ export type CfguType =
   | "LatLong"
   | "Country"
   | "Currency"
-  | "DockerImage";
+  | "DockerImage"
+  | "CreditCard";
 
 /**
  * A generic declaration of a Config, aka Cfgu that specifies information about its type and other characteristics
