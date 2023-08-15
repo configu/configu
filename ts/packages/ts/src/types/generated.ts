dcff35f7483c39fc5210758871b097e73099ca96
Sadaf-A <sadaf20043006@gmail.com>
2023-08-15 22:27:36 +0530

    feat(MACAddress): adding MACAddress Cfgu Type

--- ts/packages/ts/src/types/generated.ts
+++ ts/packages/ts/src/types/generated.ts
@@ -32,7 +32,7 @@ export interface Cfgu {
     type:         CfguType;
 }
 
-export type CfguType = "Base64" | "Boolean" | "Color" | "ConnectionString" | "Country" | "Currency" | "Domain" | "Email" | "Hex" | "IPv4" | "IPv6" | "LatLong" | "Locale" | "MD5" | "MobilePhone" | "Number" | "RegEx" | "SHA" | "SemVer" | "String" | "URL" | "UUID" | "DockerImage";
+export type CfguType = "Base64" | "Boolean" | "Color" | "ConnectionString" | "Country" | "Currency" | "Domain" | "Email" | "Hex" | "IPv4" | "IPv6" | "LatLong" | "Locale" | "MD5" | "MobilePhone" | "Number" | "RegEx" | "SHA" | "SemVer" | "String" | "URL" | "UUID" | "DockerImage" | "MACAddress";
 
 /**
  * A generic representation of a software configuration, aka Config
@@ -403,6 +403,7 @@ const typeMap: any = {
         "IPv6",
         "LatLong",
         "Locale",
+        "MACAddress",
         "MD5",
         "MobilePhone",
         "Number",
