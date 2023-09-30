9796538b9c5d3aabdf5b7de5859d6de7667ee5c0
BHARATH GADDAM <134089638+bharathgaddam1712@users.noreply.github.com>
2023-09-30 13:43:19 +0530

    Update Cfgu.ts

--- types/Cfgu.ts
+++ types/Cfgu.ts
@@ -50,3 +50,32 @@ export interface Cfgu {
   description?: string;
   options?: string[];
 }
+/** 
+  CreditCard in a ConfigSchema should enforce a valid Credit Card number (amex, visa, mastercard, etc..) as a config value at upsert or eval **/
+
+ {
+  "types": {
+    "CreditCard": {
+      "validator": "validateCreditCard"
+    }
+  },
+  "validators": {
+    "validateCreditCard": {
+      "type": "function",
+      "function": "function(value) { 
+        // Regular expression for validating credit card numbers
+        var regex = /^(?:(4[0-9]{12}(?:[0-9]{3})?)|(5[1-5][0-9]{14})|(6(?:011|5[0-9][0-9])[0-9]{12})|(3[47][0-9]{13})|(3(?:0[0-5]|[68][0-9])[0-9]{11})|((?:2131|1800|35\d{3})\d{11}))$/;
+        return regex.test(value);
+      }"
+    }
+  },
+  "schemas": {
+    "exampleSchema": {
+      "fields": {
+        "creditCardNumber": {
+          "type": "CreditCard"
+        }
+      }
+    }
+  }
+}
