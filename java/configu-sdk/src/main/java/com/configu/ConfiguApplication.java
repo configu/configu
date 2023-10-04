23f57d2689e4f1e413029b15c5ea7b0b899320b5
Zohra Khan <zohra@Zohras-MacBook-Pro.local>
2023-10-04 18:44:27 +0530

    Added spring boot app

---
+++ java/configu-sdk/src/main/java/com/configu/ConfiguApplication.java
@@ -0,0 +1,12 @@
+package com.configu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfiguApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ConfiguApplication.class, args);
+  }
+}
---
+++ java/configu-sdk/src/test/java/com/configu/ConfiguApplicationTests.java
@@ -0,0 +1,11 @@
+package com.configu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfiguApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
