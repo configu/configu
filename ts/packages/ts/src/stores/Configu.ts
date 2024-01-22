3c9b0516aeb8e65f16fc27e242fea543548cb001
Richard <richard.akman1@gmail.com>
2024-01-22 10:56:15 +0200

    chore: adjust approval queue error

--- ts/packages/ts/src/stores/Configu.ts
+++ ts/packages/ts/src/stores/Configu.ts
@@ -9,6 +9,15 @@ export type ConfiguConfigStoreConfiguration = {
   source?: string;
 };
 
+export class ConfiguConfigStoreApprovalQueueError extends Error {
+  queueUrl: string;
+  constructor(message: string, queueUrl: string) {
+    super(message);
+    this.name = 'ConfiguConfigStoreApprovalQueueError';
+    this.queueUrl = queueUrl;
+  }
+}
+
 export class ConfiguConfigStore extends ConfigStore {
   private client: Axios;
   constructor({ credentials, endpoint = `https://api.configu.com`, source = 'sdk' }: ConfiguConfigStoreConfiguration) {
@@ -39,8 +48,10 @@ export class ConfiguConfigStore extends ConfigStore {
     const response = await this.client.put('/config', { configs });
     if (response.status === 202) {
       const protectedSet = response.data.diff.pending[0].set;
-      throw new Error(
-        `Your recent upsert to the ${protectedSet} ConfigSet is currently pending in its approval queue, as ${protectedSet} is a "protected set". To proceed with these changes, please review and approve them at ${response.data.queueUrl}?sp=${protectedSet}. If you lack the necessary permissions, reach out to an authorized org member.`,
+      const queueUrl = `${response.data.queueUrl}?sp=${protectedSet}`;
+      throw new ConfiguConfigStoreApprovalQueueError(
+        `Your recent upsert to the ${protectedSet} ConfigSet is currently pending in its approval queue, as ${protectedSet} is a "protected set". To proceed with these changes, please review and approve them at ${queueUrl}. If you lack the necessary permissions, reach out to an authorized org member.`,
+        queueUrl,
       );
     }
   }
