c2db11aa0050b100feb98ecfda347588efa6ee6a
Richard <richard.akman1@gmail.com>
2024-01-25 11:40:08 +0200

    refactor: move approval queue err building to err class

--- ts/packages/ts/src/stores/Configu.ts
+++ ts/packages/ts/src/stores/Configu.ts
@@ -11,11 +11,13 @@ export type ConfiguConfigStoreConfiguration = {
 
 const ConfiguConfigStoreApprovalQueueErrorName = 'ConfiguConfigStoreApprovalQueueError';
 class ConfiguConfigStoreApprovalQueueError extends Error {
-  queueUrl: string;
+  readonly queueUrl: string;
   readonly name: string = ConfiguConfigStoreApprovalQueueErrorName;
 
-  constructor(message: string, queueUrl: string) {
-    super(message);
+  constructor(protectedSet: string, queueUrl: string) {
+    super(
+      `Your recent upsert to the ${protectedSet} ConfigSet is currently pending in its approval queue, as ${protectedSet} is a "protected set". To proceed with these changes, please review and approve them at ${queueUrl}. If you lack the necessary permissions, reach out to an authorized org member.`,
+    );
     this.queueUrl = queueUrl;
   }
 }
@@ -52,10 +54,7 @@ export class ConfiguConfigStore extends ConfigStore {
     if (response.status === 202) {
       const protectedSet = response.data.diff.pending[0].set;
       const queueUrl = `${response.data.queueUrl}?sp=${protectedSet}`;
-      throw new ConfiguConfigStoreApprovalQueueError(
-        `Your recent upsert to the ${protectedSet} ConfigSet is currently pending in its approval queue, as ${protectedSet} is a "protected set". To proceed with these changes, please review and approve them at ${queueUrl}. If you lack the necessary permissions, reach out to an authorized org member.`,
-        queueUrl,
-      );
+      throw new ConfiguConfigStoreApprovalQueueError(protectedSet, queueUrl);
     }
   }
 }
