657c5d70fd56f4e67e9be938ec86f9a4cf2ff18a
Shriekin’ Ninja <ninja@l-m-group.com>
2024-03-07 10:55:23 +0200

    Implement Local Cache for ConfigStore

--- ts/packages/node/src/stores/ORM.ts
+++ ts/packages/node/src/stores/ORM.ts
@@ -1,6 +1,14 @@
 import 'reflect-metadata';
 import { ConfigStore, type ConfigStoreQuery, type Config as IConfig } from '@configu/ts';
-import { Entity, Column, DataSource, type DataSourceOptions, Index, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Entity,
+  Column,
+  DataSource,
+  type DataSourceOptions,
+  Index,
+  PrimaryGeneratedColumn,
+  EntitySchema,
+} from 'typeorm';
 import _ from 'lodash';
 
 @Entity()
@@ -20,16 +28,57 @@ class Config {
   value: string;
 }
 
+const createTable = (tableName: string) =>
+  new EntitySchema<IConfig & { id: string }>({
+    name: tableName,
+    columns: {
+      id: {
+        type: 'uuid',
+        primary: true,
+        generated: 'uuid',
+      },
+      set: {
+        type: 'text',
+      },
+      key: {
+        type: 'text',
+      },
+      value: {
+        type: 'text',
+      },
+    },
+    indices: [
+      {
+        name: `${tableName}_IDX_SET_KEY`,
+        unique: true,
+        columns: ['set', 'key'],
+      },
+    ],
+  });
+
+type ORMConfigStoreOptions = DataSourceOptions & {
+  tables?: string[];
+};
+
 export abstract class ORMConfigStore extends ConfigStore {
   readonly dataSource: DataSource;
+  private readonly tables: Record<string, EntitySchema>;
+  private activeTable: EntitySchema;
 
-  constructor(type: string, dataSourceOptions: DataSourceOptions) {
+  constructor(type: string, { tables = [], ...dataSourceOptions }: ORMConfigStoreOptions) {
     super(type);
-
+    this.tables = _.reduce(
+      [...tables, 'config'],
+      (entities, tableName) => {
+        return { ...entities, [tableName]: createTable(tableName) };
+      },
+      {},
+    );
+    this.activeTable = this.tables.config as EntitySchema;
     this.dataSource = new DataSource({
       // TODO: synchronize is not production safe - create a migration script to initialize tables
       synchronize: true,
-      entities: [Config],
+      entities: Object.values(this.tables),
       ...dataSourceOptions,
     });
   }
@@ -38,22 +87,27 @@ export abstract class ORMConfigStore extends ConfigStore {
     await this.dataSource.initialize();
   }
 
+  useTable(tableName: string) {
+    this.activeTable = this.tables[tableName] ?? (this.tables.config as EntitySchema);
+  }
+
   private async delete(configs: IConfig[]): Promise<void> {
-    const configRepository = this.dataSource.getRepository(Config);
+    const configRepository = this.dataSource.getRepository(this.activeTable);
     const preloadedConfigs = await Promise.all(configs.map((config) => configRepository.preload(config)));
     await configRepository.delete(_.map(preloadedConfigs, 'id'));
   }
 
   private async upsert(configs: IConfig[]): Promise<void> {
-    const configRepository = this.dataSource.getRepository(Config);
+    const configRepository = this.dataSource.getRepository(this.activeTable);
 
     if (configs.length > 0) {
       await configRepository.upsert(configs, ['set', 'key']);
     }
+    // await this.dataSource.destroy();
   }
 
   async get(queries: ConfigStoreQuery[]): Promise<IConfig[]> {
-    const configRepository = this.dataSource.getRepository(Config);
+    const configRepository = this.dataSource.getRepository(this.activeTable);
 
     const adjustedQuery = queries.map((entry) => ({
       set: entry.set,
--- ts/packages/node/src/stores/SQLite.ts
+++ ts/packages/node/src/stores/SQLite.ts
@@ -1,7 +1,9 @@
 import { type SqliteConnectionOptions } from 'typeorm/driver/sqlite/SqliteConnectionOptions';
 import { ORMConfigStore } from './ORM';
 
-export type SQLiteConfigStoreConfiguration = Omit<SqliteConnectionOptions, 'type'>;
+export type SQLiteConfigStoreConfiguration = Omit<SqliteConnectionOptions, 'type'> & {
+  tables?: string[];
+};
 
 export class SQLiteConfigStore extends ORMConfigStore {
   constructor(configuration: SQLiteConfigStoreConfiguration) {
