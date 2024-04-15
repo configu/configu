2790445c19c7dcb783f99a64303e8dd286267df3
Richard Akman <richard.akman1@gmail.com>
2024-04-15 09:53:21 +0300

    Add missing stores to `@configu/lib` (#409)

--- ts/packages/lib/src/integrations/store.ts
+++ ts/packages/lib/src/integrations/store.ts
@@ -5,7 +5,10 @@ export type StoreType = LiteralUnion<
   | 'in-memory'
   | 'configu'
   | 'json-file'
+  | 'csv-file'
   | 'ini-file'
+  | 'toml-file'
+  | 'xml-file'
   | 'hashicorp-vault'
   | 'aws-parameter-store'
   | 'aws-secrets-manager'
@@ -28,7 +31,10 @@ export const STORE_LABEL: Record<StoreType, string> = {
   'in-memory': 'In Memory',
   configu: 'Configu',
   'json-file': 'Json File',
+  'csv-file': 'CSV File',
   'ini-file': 'INI File',
+  'toml-file': 'TOML File',
+  'xml-file': 'XML File',
   'hashicorp-vault': 'HashiCorp Vault',
   'aws-parameter-store': 'AWS Parameter Store',
   'aws-secrets-manager': 'AWS Secrets Manager',
@@ -50,7 +56,10 @@ export const STORE_WEBSITE: Record<StoreType, string> = {
   'in-memory': '',
   configu: 'https://configu.com/',
   'json-file': 'https://www.json.org/json-en.html',
+  'csv-file': 'https://en.wikipedia.org/wiki/Comma-separated_values',
   'ini-file': 'https://en.wikipedia.org/wiki/INI_file',
+  'toml-file': 'https://toml.io',
+  'xml-file': 'https://www.w3.org/XML',
   'hashicorp-vault': 'https://www.vaultproject.io/',
   'aws-parameter-store': 'https://aws.amazon.com/systems-manager/features/#Parameter_Store',
   'aws-secrets-manager': 'https://aws.amazon.com/secrets-manager/',
