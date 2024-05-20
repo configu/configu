e8997fa1b283ea4d207c5af560ee423aaca684b8
Ran Cohen <rannn505@outlook.com>
2024-05-20 17:08:02 +0300

    scripts

--- docs/prepare.mjs
+++ docs/prepare.mjs
@@ -1,4 +1,6 @@
 #!/usr/bin/env pnpm zx
+/* eslint-disable no-undef */
+/* eslint-disable import/no-extraneous-dependencies */
 
 import 'zx/globals';
 import _ from 'lodash';
@@ -13,21 +15,21 @@ export const confirm = async (q) => {
   const answer = await question(`${q} y(es)/n(o) `); // Y(all)/N(none)/q(uit)
   if (answer.toLowerCase() === 'yes' || answer.toLowerCase() === 'y') {
     return true;
-  } else if (answer.toLowerCase() === 'no' || answer.toLowerCase() === 'n') {
-    return false;
-  } else {
-    // Recurse to re-ask the same question if input is invalid
-    console.log("Invalid input");
-    return confirm(q);
   }
-}
+  if (answer.toLowerCase() === 'no' || answer.toLowerCase() === 'n') {
+    return false;
+  }
+  // Recurse to re-ask the same question if input is invalid
+  console.log('Invalid input');
+  return confirm(q);
+};
 
 // Write the integrations index.json data to the snippets/index.mdx file as a variable
 const INTEGRATIONS_INDEX_PATH = path.join(DOCS_ROOT_PATH, 'integrations', 'index.json');
 const INTEGRATIONS_INDEX_CONTENT = await fs.readJson(INTEGRATIONS_INDEX_PATH);
 
 const INTEGRATIONS_MDX_PATH = path.join(DOCS_ROOT_PATH, 'snippets', 'index.mdx');
-const INTEGRATIONS_INDEX_MDX_CONTENT = `export const integrations = ${JSON.stringify(INTEGRATIONS_INDEX_CONTENT, null, 2)};`
+const INTEGRATIONS_INDEX_MDX_CONTENT = `export const integrations = ${JSON.stringify(INTEGRATIONS_INDEX_CONTENT, null, 2)};`;
 
 await fs.writeFile(INTEGRATIONS_MDX_PATH, INTEGRATIONS_INDEX_MDX_CONTENT, { flag: 'w' });
 
@@ -38,35 +40,37 @@ const integrationsNavArray = _(INTEGRATIONS_INDEX_CONTENT)
     const [group, subgroup] = cur.group;
 
     // Initialize the group if it doesn't exist
-    if (!acc.some(item => item.group === group)) {
+    if (!acc.some((item) => item.group === group)) {
       acc.push({ group, pages: [] });
     }
 
     // If there is no subgroup, push the docs to the group
     if (!subgroup) {
-      acc.find(item => item.group === group).pages.push(cur.docs, ...(cur.pages ?? []));
+      acc.find((item) => item.group === group).pages.push(cur.docs, ...(cur.pages ?? []));
       return acc;
     }
 
     // Initialize the subgroup if it doesn't exist
-    if (!acc.find(item => item.group === group).pages.some(item => item.group === subgroup)) {
-      acc.find(item => item.group === group).pages.push({ group: subgroup, pages: [] });
+    if (!acc.find((item) => item.group === group).pages.some((item) => item.group === subgroup)) {
+      acc.find((item) => item.group === group).pages.push({ group: subgroup, pages: [] });
     }
 
     // Push the current integration docs to the correct subgroup
-    acc.find(item => item.group === group).pages.find(item => item.group === subgroup).pages.push(cur.docs, ...(cur.pages ?? []));
+    acc
+      .find((item) => item.group === group)
+      .pages.find((item) => item.group === subgroup)
+      .pages.push(cur.docs, ...(cur.pages ?? []));
 
     return acc;
   }, []);
 
-
 // Update the mint.json file with the new integrations navigation data
 const MINT_CONTENT = await fs.readJson(MINT_PATH);
 
-const INTEGRATIONS_NAV = MINT_CONTENT.navigation.find(item => item.group === 'Integrations');
-INTEGRATIONS_NAV.pages = ["integrations/overview", ...integrationsNavArray];
+const INTEGRATIONS_NAV = MINT_CONTENT.navigation.find((item) => item.group === 'Integrations');
+INTEGRATIONS_NAV.pages = ['integrations/overview', ...integrationsNavArray];
 
-await fs.writeJson(path.join(DOCS_ROOT_PATH, 'mint.json'), MINT_CONTENT, { spaces: 2});
+await fs.writeJson(path.join(DOCS_ROOT_PATH, 'mint.json'), MINT_CONTENT, { spaces: 2 });
 
 // Process README files to create the MDX files for the docs
 const prepareReadme = async ({ source, target, title = 'Overview' }) => {
@@ -152,39 +156,44 @@ await prepareReadme({
 // Check the navigation structure for missing pages
 const missingPages = [];
 const checkNavigation = async (navigation) => {
-  for (const item of navigation) {
-    if (typeof item === 'object') {
-      await checkNavigation(item.pages);
-    } else {
-      const pagePath = path.join(DOCS_ROOT_PATH, `${item}${PAGE_EXT}`);
-      const exists = await fs.pathExists(pagePath);
-      if (!exists) {
-        const baseName = path.basename(pagePath, PAGE_EXT);
-        echo(`Missing page: ${baseName}`);
-        missingPages.push(pagePath);
+  await Promise.all(
+    navigation.map(async (item) => {
+      if (typeof item === 'object') {
+        await checkNavigation(item.pages);
+      } else {
+        const pagePath = path.join(DOCS_ROOT_PATH, `${item}${PAGE_EXT}`);
+        const exists = await fs.pathExists(pagePath);
+        if (!exists) {
+          const baseName = path.basename(pagePath, PAGE_EXT);
+          echo(`Missing page: ${baseName}`);
+          missingPages.push(pagePath);
+        }
       }
-    }
-  }
+    }),
+  );
 };
 
 await checkNavigation(MINT_CONTENT.navigation);
 
-for (const page of missingPages) {
+missingPages.forEach(async (page) => {
   const answer = await confirm(`The page "${page}" is missing.\nWould you like to create it? `);
   if (answer) {
     // Create the missing page nested in the correct directory structure
     const pageDir = path.dirname(page);
     console.log(pageDir);
     await fs.mkdir(pageDir, { recursive: true });
-    await fs.writeFile(page, `---
+    await fs.writeFile(
+      page,
+      `---
 // TODO: Add content to this page
 ---
 
 import { Construction } from '/snippets/callouts.mdx'
 
-<Construction />`);
+<Construction />`,
+    );
   }
-}
+});
 
 // Check for broken links in the docs
 await $`pnpm mintlify broken-links`.pipe(process.stdout);
--- lint-staged.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  'packages/**/*.{js,ts}?(x)': (filenames) => [
-    'pnpm build',
-    `pnpm eslint ${filenames.join(' ')} --ext js,ts --cache --ignore-path .gitignore`,
-    `pnpm prettier ${filenames.join(' ')} --check --ignore-path .gitignore`,
-  ],
-};
