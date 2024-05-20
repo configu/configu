85f81d47e7ca3abd1001809c99440a415316f733
Richard <richard.akman1@gmail.com>
2024-05-20 15:39:50 +0300

    chore: improve app container components composition

--- ts/packages/ui/src/components/AppContainer.tsx
+++ ts/packages/ui/src/components/AppContainer.tsx
@@ -1,24 +1,31 @@
 import React from 'react';
+import { cn } from '../lib/utils';
 
-const AppContainer = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>((props, ref) => (
-  <div ref={ref} className="h-screen w-screen flex flex-col bg-blue dark:bg-blue-700" {...props}>
-    {props.children}
-  </div>
-));
+const AppContainer = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+  ({ className, ...props }, ref) => (
+    <div ref={ref} className={cn('h-screen w-screen flex flex-col bg-blue dark:bg-blue-700', className)} {...props}>
+      {props.children}
+    </div>
+  ),
+);
 AppContainer.displayName = 'AppContainer';
 
-const Header = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>((props, ref) => (
-  <div ref={ref} className="flex-1 mt-3 mx-[13px] rounded-4xl bg-white dark:bg-black" {...props}>
-    {props.children}
-  </div>
-));
+const Header = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+  ({ className, ...props }, ref) => (
+    <div ref={ref} className={cn('flex-1 mt-3 mx-[13px] rounded-4xl bg-white dark:bg-black', className)} {...props}>
+      {props.children}
+    </div>
+  ),
+);
 Header.displayName = 'Header';
 
-const Footer = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>((props, ref) => (
-  <div ref={ref} className="h-[74px]" {...props}>
-    {props.children}
-  </div>
-));
+const Footer = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+  ({ className, ...props }, ref) => (
+    <div ref={ref} className={cn('h-[74px]', className)} {...props}>
+      {props.children}
+    </div>
+  ),
+);
 Footer.displayName = 'Footer';
 
 export { AppContainer, Header, Footer };
