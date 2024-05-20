6cbcf2d3b9e6982463c6a5233e0e17fc2199f515
Richard <richard.akman1@gmail.com>
2024-05-20 10:55:29 +0300

    chore: more dark theme adjustments to switch

--- ts/packages/ui/src/components/Switch.tsx
+++ ts/packages/ui/src/components/Switch.tsx
@@ -8,10 +8,10 @@ const Switch = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <SwitchPrimitives.Root
     className={cn(
-      'peer inline-flex h-6 w-11 cursor-pointer items-center rounded-full border-2 border-switch data-[state=unchecked]:bg-switch',
+      'peer inline-flex h-6 w-11 cursor-pointer items-center rounded-full border-2 border-gray-300 data-[state=unchecked]:bg-gray-300',
       'data-[state=checked]:border-blue data-[state=checked]:bg-blue',
       'dark:data-[state=unchecked]:bg-gray-700 dark:data-[state=unchecked]:border-gray-700',
-      'disabled:cursor-not-allowed disabled:opacity-50',
+      'disabled:cursor-not-allowed disabled:data-[state=unchecked]:bg-gray-200 disabled:border-gray-200',
       className,
     )}
     {...props}
@@ -20,6 +20,7 @@ const Switch = React.forwardRef<
     <SwitchPrimitives.Thumb
       className={cn(
         'pointer-events-none block h-[18px] w-[18px] rounded-full bg-white transition-transform data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0.5',
+        'data-[disabled]:bg-gray-100 dark:data-[disabled]:bg-gray-300',
       )}
     />
   </SwitchPrimitives.Root>
--- ts/packages/ui/tailwind.config.ts
+++ ts/packages/ui/tailwind.config.ts
@@ -83,7 +83,6 @@ const config = {
       colors: {
         border: 'hsl(var(--border))',
         input: 'hsl(var(--input))',
-        switch: 'hsl(var(--switch))',
         ring: 'hsl(var(--ring))',
         background: 'hsl(var(--background))',
         foreground: 'hsl(var(--foreground))',
