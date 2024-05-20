9704c05565a4a043fbfa8c5a06a21ac6677c0526
Richard <richard.akman1@gmail.com>
2024-05-20 10:27:54 +0300

    chore: adjust switch for dark theme

--- ts/packages/ui/src/components/Switch.tsx
+++ ts/packages/ui/src/components/Switch.tsx
@@ -2,14 +2,16 @@ import * as React from 'react';
 import * as SwitchPrimitives from '@radix-ui/react-switch';
 import { cn } from '../lib/utils';
 
-// TODO: add dark mode design
 const Switch = React.forwardRef<
   React.ElementRef<typeof SwitchPrimitives.Root>,
   React.ComponentPropsWithoutRef<typeof SwitchPrimitives.Root>
 >(({ className, ...props }, ref) => (
   <SwitchPrimitives.Root
     className={cn(
-      'peer inline-flex h-6 w-11 cursor-pointer items-center rounded-full border-2 border-switch disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:border-blue data-[state=checked]:bg-blue data-[state=unchecked]:bg-switch',
+      'peer inline-flex h-6 w-11 cursor-pointer items-center rounded-full border-2 border-switch data-[state=unchecked]:bg-switch',
+      'data-[state=checked]:border-blue data-[state=checked]:bg-blue',
+      'dark:data-[state=unchecked]:bg-gray-700 dark:data-[state=unchecked]:border-gray-700',
+      'disabled:cursor-not-allowed disabled:opacity-50',
       className,
     )}
     {...props}
