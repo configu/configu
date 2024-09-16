b2956840557938c6a56a7d257e763df73bd93dc5
Richard <richard.akman1@gmail.com>
2024-09-16 10:59:44 +0300

    feat: add caching feature to eval command in configu common

--- packages/common/src/commands/EvalCommand.ts
+++ packages/common/src/commands/EvalCommand.ts
@@ -1,7 +1,17 @@
-import { EvalCommand as BaseEvalCommand, type EvalCommandParameters as BaseEvalCommandParameters } from '@configu/ts';
+import {
+  EvalCommand as BaseEvalCommand,
+  ConfigStore,
+  ConfigStoreError,
+  EvalCommandReturn,
+  EvaluatedConfigOrigin,
+  UpsertCommand,
+  type EvalCommandParameters as BaseEvalCommandParameters,
+} from '@configu/ts';
+import _ from 'lodash';
 
 export type EvalCommandParameters = BaseEvalCommandParameters & {
-  override?: boolean;
+  cacheStore?: ConfigStore;
+  forceCache?: boolean;
 };
 
 export class EvalCommand extends BaseEvalCommand {
@@ -9,9 +19,39 @@ export class EvalCommand extends BaseEvalCommand {
     super(parameters);
   }
 
+  async updateCacheStore(result: EvalCommandReturn) {
+    const { schema, set, cacheStore } = this.parameters;
+    if (!cacheStore) return;
+
+    const configs = _.mapValues(
+      _.pickBy(result, (entry) => entry.result.origin === EvaluatedConfigOrigin.StoreSet),
+      (entry) => entry.result.value,
+    );
+    await new UpsertCommand({ store: cacheStore, set, schema, configs }).run();
+  }
+
   async run() {
-    const res = await super.run();
-    // TODO: Extract caching logic from CLI
-    return res;
+    let evalCommandReturn: EvalCommandReturn;
+
+    if (this.parameters.cacheStore && !this.parameters.forceCache) {
+      try {
+        evalCommandReturn = await super.run();
+        await this.updateCacheStore(evalCommandReturn);
+      } catch (error) {
+        if (error instanceof ConfigStoreError) {
+          this.parameters.store = this.parameters.cacheStore;
+          evalCommandReturn = await super.run();
+        } else {
+          throw error;
+        }
+      }
+    } else {
+      if (this.parameters.forceCache && this.parameters.cacheStore) {
+        this.parameters.store = this.parameters.cacheStore;
+      }
+      evalCommandReturn = await super.run();
+    }
+
+    return evalCommandReturn;
   }
 }
