56196a3ba27368a6f35631064b5d1fb5938a46a5
mila2999 <mila.savonik@gmail.com>
2024-09-28 12:29:00 +0300

    Created two tests for ConfigSet

--- packages/ts/src/ConfigSet.test.ts
+++ packages/ts/src/ConfigSet.test.ts
@@ -1,15 +1,41 @@
 import { describe, test } from 'node:test';
 import assert from 'node:assert/strict';
 import { ConfigSet } from './ConfigSet';
+import { ConfigError } from './utils';
 
-describe.only(`ConfigSet`, () => {
+describe(`ConfigSet`, () => {
   describe(`constructor`, () => {
-    test(`treats ROOT_LABEL as ROOT`, async () => {
-      const set = new ConfigSet(ConfigSet.ROOT_LABEL);
+    test(`uses ROOT path by default`, () => {
+      const set = new ConfigSet();
       assert.strictEqual(set.path, ConfigSet.ROOT);
       assert.deepStrictEqual(set.hierarchy, [ConfigSet.ROOT]);
-      // expect(set.path).toBe(ConfigSet.ROOT);
-      // expect(set.hierarchy).toEqual([ConfigSet.ROOT]);
+    });
+
+    test(`Should throw an error when the path includes special characters.`, () => {
+      assert.throws(
+        () => new ConfigSet(`some-path${ConfigSet.SEPARATOR}`),
+        (error) => {
+          assert.ok(error instanceof ConfigError);
+          assert.match(error.message, new RegExp(`path mustn't end with ${ConfigSet.SEPARATOR} character`));
+
+          return true;
+        },
+      );
+    });
+
+    test(`Should throw an error when the path includes reserved names ('_', '-', 'this', 'cfgu')`, () => {
+      const reservedNames = ['_', '-', 'this', 'cfgu'];
+      reservedNames.forEach((name) => {
+        assert.throws(
+          () => new ConfigSet(name),
+          (error) => {
+            assert.ok(error instanceof ConfigError);
+            assert.match(error.message, new RegExp(`path nodes mustn't contain reserved words "${name}"`));
+
+            return true;
+          },
+        );
+      });
     });
   });
 });
