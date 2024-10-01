01ee2e58157bd26fbee4fc8bdb6b93ae1c12da1e
Michał Dziuba <mail@michaldziuba.dev>
2024-10-01 21:52:35 +0200

    simplify formatters test

--- packages/lib/src/formatters/formatters.test.ts
+++ packages/lib/src/formatters/formatters.test.ts
@@ -1,53 +1,30 @@
 import { describe, test } from 'node:test';
 import { equal } from 'node:assert/strict';
-import { ConfigSchema, EvalCommand, InMemoryConfigStore, ConfigSet, ExportCommand } from '@configu/ts';
 import { formatConfigs } from '.';
 
 describe('Formatters', () => {
-  const getEvalResult = async () =>
-    new EvalCommand({
-      store: new InMemoryConfigStore(),
-      set: new ConfigSet('test'),
-      schema: new ConfigSchema('mutate', {
-        KEY0: {
-          type: 'String',
-        },
-        KEY1: {
-          type: 'String',
-        },
-      }),
-      configs: {
-        KEY0: 'KEY0',
-        KEY1: 'KEY1',
-      },
-    }).run();
-
-  const getExport = async () => {
-    const evalResult = await getEvalResult();
-    return new ExportCommand({ pipe: evalResult }).run();
+  const json = {
+    KEY0: 'KEY0',
+    KEY1: 'KEY1',
   };
 
   test('should export to dotenv format', async () => {
-    const exportResult = await getExport();
-    const formatted = formatConfigs({ json: exportResult, format: 'Dotenv', label: 'test' });
+    const formatted = formatConfigs({ json, format: 'Dotenv', label: 'test' });
     equal(formatted, `KEY0=KEY0\nKEY1=KEY1`);
   });
 
   test('should export to ini format', async () => {
-    const exportResult = await getExport();
-    const formatted = formatConfigs({ json: exportResult, format: 'INI', label: 'test' });
+    const formatted = formatConfigs({ json, format: 'INI', label: 'test' });
     equal(formatted, `KEY0=KEY0\nKEY1=KEY1\n`);
   });
 
   test('should export to yaml format', async () => {
-    const exportResult = await getExport();
-    const formatted = formatConfigs({ json: exportResult, format: 'YAML', label: 'test' });
+    const formatted = formatConfigs({ json, format: 'YAML', label: 'test' });
     equal(formatted, 'KEY0: KEY0\nKEY1: KEY1\n');
   });
 
   test('should export to json format', async () => {
-    const exportResult = await getExport();
-    const formatted = formatConfigs({ json: exportResult, format: 'JSON', label: 'test' });
+    const formatted = formatConfigs({ json, format: 'JSON', label: 'test' });
     equal(formatted, '{\n  "KEY0": "KEY0",\n  "KEY1": "KEY1"\n}');
   });
 });
