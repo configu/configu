49be041d96466957f0b62f781391958304695414
David Meir-Levy <david@davidlevy.co.il>
2024-10-07 11:18:29 +0300

    493 - convert integrations to import sdk instead of ts lib

--- packages/integrations/stores/aws-parameter/tsup.config.ts
+++ packages/integrations/stores/aws-parameter/tsup.config.ts
@@ -1,20 +1,18 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { defineConfig, Options } from 'tsup';
-import { readdir, readFile } from 'node:fs/promises';
+import { readdir } from 'node:fs/promises';
 import { platform } from 'node:os';
 
 const osName = process.env.OS_NAME || platform();
 
 export default defineConfig(async (): Promise<Options | Options[]> => {
-  const pkg = JSON.parse(await readFile('package.json', 'utf-8'));
-  const noExternal = Object.keys(pkg.dependencies || {}).filter((dep) => !dep.startsWith('@configu'));
-
   const files = await readdir('src');
 
   return {
     entry: files.filter((file) => file.endsWith('.ts') && !file.includes('.test.')).map((file) => `src/${file}`),
     target: 'esnext',
-    noExternal,
+    format: 'esm',
+    noExternal: [/(.*)/],
     outDir: `../../dist`,
     outExtension: () => ({
       js: `.os-${osName}.js`,
