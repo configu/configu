5614fac6b22254747786414171c8a34a5e39bbcd
David Meir-Levy <david@davidlevy.co.il>
2024-10-07 12:57:27 +0300

    491 - remote registry of integrations from release (#492)

--- packages/common/src/Registry.ts
+++ packages/common/src/Registry.ts
@@ -5,6 +5,8 @@ import path from 'pathe';
 import { tsImport } from 'tsx/esm/api';
 import { ConfigStore, ConfigStoreConstructor, Expression, ExpressionFunction } from '@configu/sdk';
 
+const CONFIGU_HOME = path.join(process.cwd(), '/.configu-cache');
+
 export class Registry {
   private static store = new Map<string, ConfigStoreConstructor>();
 
@@ -14,6 +16,14 @@ export class Registry {
 
   static isExpression(value: unknown): value is ExpressionFunction<any[], any> {
     return typeof value === 'function';
+  }
+
+  private static async ensureCacheDir() {
+    try {
+      await mkdir(CONFIGU_HOME, { recursive: true });
+    } catch {
+      // ignore
+    }
   }
 
   static async import(filePath: string) {
@@ -42,21 +52,23 @@ export class Registry {
   }
 
   static async remoteRegister(key: string) {
-    const VERSION = 'latest';
-    const CONFIGU_HOME = path.join(process.cwd(), '/.configu-cache');
+    if (Registry.store.has(key)) {
+      return;
+    }
+    await Registry.ensureCacheDir();
 
-    const MODULE_PATH = path.join(CONFIGU_HOME, `/${key}-${VERSION}.js`);
+    const [KEY, VERSION = 'latest'] = key.split('@');
+    const MODULE_PATH = path.join(CONFIGU_HOME, `/${KEY}-${VERSION}.js`);
 
     if (!existsSync(MODULE_PATH)) {
       const res = await fetch(
-        `https://github.com/configu/configu/releases/download/${VERSION}/${key}-${platform()}.js`,
+        `https://github.com/configu/configu/releases/download/integrations-${VERSION}/${KEY}-${platform()}.js`,
       );
       if (res.ok) {
-        await mkdir(CONFIGU_HOME, { recursive: true });
         await writeFile(MODULE_PATH, await res.text());
       }
     }
-    Registry.register(MODULE_PATH);
+    await Registry.register(MODULE_PATH);
   }
 
   static constructStore(type: string, configuration = {}): ConfigStore {
