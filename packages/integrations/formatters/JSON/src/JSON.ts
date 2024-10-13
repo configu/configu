81893681463e0d2aaebd5bca6fbef177f3dfae94
David Meir-Levy <david@davidlevy.co.il>
2024-10-13 09:40:20 +0300

    initial load of integrations that are included inside the cli and proxy

--- packages/integrations/formatters/JSON/src/JSON.ts
+++ packages/integrations/formatters/JSON/src/JSON.ts
@@ -1,2 +1,2 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-export const toJson = ({ json }) => JSON.stringify(json, null, 2);
+export const JSONExpression = ({ json }) => JSON.stringify(json, null, 2);
