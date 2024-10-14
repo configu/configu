817e2f5392919c3e4aa344b0f11d7b89b61d69b9
Richard <richard.akman1@gmail.com>
2024-10-14 10:40:07 +0300

    chore: remove login command for now

--- packages/cli/src/commands/base.ts
+++ packages/cli/src/commands/base.ts
@@ -2,27 +2,9 @@ import { Command, Option } from 'clipanion';
 import _ from 'lodash';
 import { CfguFile, ConfiguFile, parseJSON, Registry } from '@configu/common';
 import { EvalCommandOutput } from '@configu/sdk';
-import path from 'path';
 import { type CustomContext } from '../index';
-import { getConfigDir } from '../helpers';
 
-export type Context = CustomContext & {
-  configu: ConfiguFile;
-  credentials: {
-    file: string; // $HOME/.config/configu/config.json
-    // TODO: consider getting this from ConfiguConfigStoreConfiguration
-    data:
-      | {
-          credentials: { org: string; token: string };
-          endpoint?: string;
-          source?: string;
-          tag?: string;
-        }
-      | Record<string, never>;
-  };
-  UNICODE_NULL: '\u0000';
-  stdin: NodeJS.ReadStream;
-};
+export type Context = CustomContext & { configu: ConfiguFile; UNICODE_NULL: '\u0000'; stdin: NodeJS.ReadStream };
 
 export abstract class BaseCommand extends Command<Context> {
   public async init(): Promise<void> {
@@ -30,10 +12,6 @@ export abstract class BaseCommand extends Command<Context> {
 
     const configu = await ConfiguFile.search();
     this.context.configu = configu;
-    this.context.credentials = {
-      file: path.join(getConfigDir(), 'config.json'),
-      data: {},
-    };
   }
 
   getBackupStoreInstanceByFlag(flag?: string) {
