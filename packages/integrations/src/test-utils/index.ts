57b688d3bc38ac3f09cd74fc9125dfa732ffc6d0
David Meir-Levy <david@davidlevy.co.il>
2024-10-20 17:59:47 +0300

    better test utils interface

--- packages/integrations/src/test-utils/index.ts
+++ packages/integrations/src/test-utils/index.ts
@@ -1,26 +1,38 @@
 import test, { describe, beforeEach } from 'node:test';
 import * as assert from 'node:assert';
+import type { Config } from '@configu/sdk';
 import { Test, TestBed } from './test-bed';
 
 describe('example', () => {
   let module: TestBed;
   const mockStore = Test.createMockStore();
 
-  function myExpression(str: string) {
+  const myExpression = test.mock.fn((str: string) => {
     return str.includes('foo');
-  }
+  });
+
+  const DotEnv = test.mock.fn((data: Record<string, Config>) => {
+    return Object.keys(data)
+      .map((key) => `${key}=${(data[key] as any)?.value}`)
+      .join('\n');
+  });
 
   beforeEach(async () => {
     module = await Test.createTestingBed({
       schema: Test.createMockSchema(['FOO', 'BAR']),
       set: 'default',
       stores: [mockStore],
-      expressions: [...Test.createMockExpressions(['foo', 'bar']), { key: 'myExpression', value: myExpression }],
+      expressions: [
+        ...Test.createMockExpressions(['foo', 'bar']),
+        { key: 'myExpression', value: myExpression },
+        { key: 'MyDotEnv', value: DotEnv },
+      ],
     });
   });
 
   test('test', async () => {
     assert.equal(mockStore.getMock.callCount(), 0);
+    assert.equal(myExpression.mock.callCount(), 0);
 
     mockStore.getMock.mockImplementation(async () => {
       return [
@@ -33,6 +45,7 @@ describe('example', () => {
       BAR: { key: 'BAR', value: 'false', set: 'default' },
     });
     assert.equal(mockStore.getMock.callCount(), 1);
+    assert.equal(myExpression.mock.callCount(), 2); // run for each key
 
     mockStore.getMock.mockImplementation(async () => {
       return [
@@ -45,13 +58,16 @@ describe('example', () => {
       BAR: { key: 'BAR', value: 'true', set: 'default' },
     });
     assert.equal(mockStore.getMock.callCount(), 2);
+    assert.equal(myExpression.mock.callCount(), 4);
   });
 
   test('eval test', async () => {
+    assert.equal(DotEnv.mock.callCount(), 0);
     assert.equal(mockStore.getMock.callCount(), 2);
 
-    assert.equal(await module.eval('Dotenv()'), `FOO=foo\nBAR=bar`);
+    assert.equal(await module.eval('MyDotEnv()'), `FOO=foo\nBAR=bar`);
     assert.equal(mockStore.getMock.callCount(), 3);
+    assert.equal(DotEnv.mock.callCount(), 1);
   });
 
   test('upsert test', async () => {
