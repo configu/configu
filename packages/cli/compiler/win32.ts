28abcfd166657399f8b4e47748ffc0413854721b
David Meir-Levy <david@davidlevy.co.il>
2024-11-04 22:52:05 +0200

    ignore signature

--- packages/cli/compiler/win32.ts
+++ packages/cli/compiler/win32.ts
@@ -44,12 +44,12 @@ async function getSigntoolLocation() {
 export async function run(filename = 'configu') {
   execSync('node --experimental-sea-config sea-config.json');
   copyFileSync(process.execPath, 'configu.exe');
-  const signtool = await getSigntoolLocation();
-  execSync(`"${signtool}" remove /s configu.exe`);
+  // const signtool = await getSigntoolLocation();
+  // execSync(`"${signtool}" remove /s configu.exe`);
   execSync(
     'pnpx postject configu.exe NODE_SEA_BLOB sea-prep.blob ^' +
       '    --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2',
   );
-  execSync(`"${signtool}" sign /fd SHA256 configu.exe`);
+  // execSync(`"${signtool}" sign /fd SHA256 configu.exe`);
   execSync(`move configu.exe dist/${filename}.exe`);
 }
