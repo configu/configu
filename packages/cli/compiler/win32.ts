fe56a3d366496c5818e3f76f5492c0fd18f235c4
David Meir-Levy <david@davidlevy.co.il>
2024-11-04 22:39:19 +0200

    install signtool on windows build

--- packages/cli/compiler/win32.ts
+++ packages/cli/compiler/win32.ts
@@ -44,7 +44,7 @@ async function getSigntoolLocation() {
 export async function run(filename = 'configu') {
   execSync('node --experimental-sea-config sea-config.json');
   copyFileSync(process.execPath, 'configu.exe');
-  execSync(`${await getSigntoolLocation()} remove /s configu.exe`);
+  execSync(`"${await getSigntoolLocation()}" remove /s configu.exe`);
   execSync(
     'pnpx postject configu.exe NODE_SEA_BLOB sea-prep.blob ^' +
       '    --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2',
