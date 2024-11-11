d6f02b06329274c92f08dd388ac144df48cffb2a
David Meir-Levy <david@davidlevy.co.il>
2024-11-11 11:34:56 +0200

    build with arch

--- packages/cli/compiler/index.ts
+++ packages/cli/compiler/index.ts
@@ -1,7 +1,8 @@
-import { platform } from 'node:os';
+import { arch, platform } from 'node:os';
 import { compress } from './gz';
 
 const os = platform();
+const osArch = arch();
 
 console.log('Platform:', os);
 
@@ -9,7 +10,7 @@ async function compile() {
   // eslint-disable-next-line no-useless-concat
   const { run } = await import('./' + `${os}.ts`);
 
-  const fileName = `configu-${os}`;
+  const fileName = `configu-${os}-${osArch}`;
   const filePath = await run(fileName);
 
   console.log('app compiled:', filePath);
