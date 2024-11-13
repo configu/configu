5cff05133967eb4be7edec1933c35750d144edd6
David Meir-Levy <david@davidlevy.co.il>
2024-11-13 12:19:53 +0200

    build for all archs

--- packages/cli/compiler/darwin.ts
+++ packages/cli/compiler/darwin.ts
@@ -1,8 +1,11 @@
 import { execSync } from 'node:child_process';
 
-export function run(filename = 'configu') {
+export function run(filename: string, nodePath: string) {
+  // eslint-disable-next-line no-param-reassign
+  filename = filename || 'configu';
+
   execSync('node --experimental-sea-config sea-config.json');
-  execSync('cp $(command -v node) configu');
+  execSync(`cp ${nodePath} configu`);
   execSync('codesign --remove-signature configu');
   execSync(
     'pnpx postject configu NODE_SEA_BLOB sea-prep.blob ' +
