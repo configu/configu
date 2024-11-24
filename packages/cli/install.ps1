6173e76690efc2be401c92b5a4ed239abb6db102
David Meir-Levy <david@davidlevy.co.il>
2024-11-24 13:20:17 +0200

    wip

--- packages/cli/install.ps1
+++ packages/cli/install.ps1
@@ -28,6 +28,8 @@ Write-Output "Installing Configu v${Version} for ${Target}..."
 
 $DownloadUrl = "https://github.com/configu/configu/releases/download/cli%2Fv${Version}/configu-${Target}.exe"
 
+Write-Output "Downloading Configu from: ${DownloadUrl}"
+
 if (!(Test-Path $BinDir)) {
   New-Item $BinDir -ItemType Directory | Out-Null
 }
