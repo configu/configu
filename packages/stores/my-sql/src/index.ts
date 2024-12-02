1561cb3928753ff209caeb9a75844020257f840d
David Meir-Levy <david@davidlevy.co.il>
2024-12-02 11:54:36 +0200

    #674 | V1 | move all integrations/stores to stores lib - my-sql

--- packages/stores/my-sql/src/index.ts
+++ packages/stores/my-sql/src/index.ts
@@ -1,5 +1,5 @@
 import { type MysqlConnectionOptions } from 'typeorm/driver/mysql/MysqlConnectionOptions.js';
-import { ORMConfigStore, type ORMConfigStoreSharedConfiguration } from '@configu/integrations/src/utils/ORM';
+import { ORMConfigStore, type ORMConfigStoreSharedConfiguration } from '@configu/database';
 
 export type MySQLConfigStoreConfiguration = Omit<MysqlConnectionOptions, 'type'> & ORMConfigStoreSharedConfiguration;
 
