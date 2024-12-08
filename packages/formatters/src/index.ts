bf8e9852069f36943d2a82531c826051ecdfcd86
Richard <richard.akman1@gmail.com>
2024-12-08 13:34:42 +0200

    feat: add missing formatters for INI, Kubernetes ConfigMap, and Terraform tfvars

--- packages/formatters/src/index.ts
+++ packages/formatters/src/index.ts
@@ -2,7 +2,10 @@ import './JSON';
 import './YAML';
 import './Dotenv';
 import './TOML';
+import './INI';
 import './HelmValues';
+import './TerraformTfvars';
+import './KubernetesConfigMap';
 
 // todo: restore rest of the formatters
 export * from './ConfigFormatter';
