ab3ab35ee871043c001a0095920ec6b5b8ff6afd
BeGoCode <bennygordin@gmail.com>
2025-01-14 09:33:34 +0200

    Fixed tests

--- packages/proxy/src/__tests__/proxy.test.ts
+++ packages/proxy/src/__tests__/proxy.test.ts
@@ -1,122 +1,152 @@
-// import assert from 'assert';
-// import { test, before, after } from 'node:test';
-// import http from 'http';
-// import { buildServer } from '../server';
+import assert from 'assert';
+import { test, before, after } from 'node:test';
+import http from 'http';
+import { AddressInfo } from 'net';
+import { buildServer } from '../server';
 
-// let server: ReturnType<typeof buildServer>;
+let server: ReturnType<typeof buildServer>;
 
-// before(async () => {
-//   server = buildServer();
-//   await server.ready();
-// });
+type HttpResponse = {
+  status: number;
+  headers: Record<string, string>;
+  body: string;
+};
 
-// after(async () => {
-//   await server.close();
-// });
+function isHttpResponse(obj: unknown): obj is HttpResponse {
+  return typeof obj === 'object' && obj !== null && 'status' in obj && 'headers' in obj && 'body' in obj;
+}
 
-// test('should respond with 200 on GET /docs (if enabled)', async () => {
-//   const address = server.server.address();
-//   if(!address)
-//     throw new Error('No address');
+before(async () => {
+  server = buildServer();
+  await server.ready();
+});
 
-//   const options = {
-//     hostname: 'localhost',
-//     port: address.port,
-//     path: '/docs',
-//     method: 'GET',
-//   };
+after(async () => {
+  await server.close();
+});
 
-//   const response = await new Promise((resolve, reject) => {
-//     const req = http.request(options, (res) => {
-//       let data = '';
-//       res.on('data', (chunk) => (data += chunk));
-//       res.on('end', () => resolve({ status: res.statusCode, text: data }));
-//     });
-//     req.on('error', reject);
-//     req.end();
-//   });
+test('should respond with 200 on GET /docs (if enabled)', async () => {
+  const address = server.server.address() as AddressInfo;
 
-//   assert.strictEqual(response.status, 200, 'Expected status code 200');
-//   assert.match(response.text, /html/, 'Expected response to include "html"');
-// });
+  const options = {
+    hostname: 'localhost',
+    port: address.port,
+    path: '/docs',
+    method: 'GET',
+  };
 
-// test('should handle POST /export successfully', async () => {
-//   const mockBody = JSON.stringify([
-//     {
-//       store: 'testStore',
-//       set: 'testSet',
-//       schema: { keys: { SOME_KEY: 'string' } },
-//       configs: { SOME_KEY: 'some value' },
-//     },
-//   ]);
+  const response = await new Promise((resolve, reject) => {
+    const req = http.request(options, (res) => {
+      let data = '';
+      res.on('data', (chunk) => {
+        data += chunk;
+      });
+      res.on('end', () => resolve({ status: res.statusCode, text: data }));
+    });
+    req.on('error', reject);
+    req.end();
+  });
 
-//   const options = {
-//     hostname: 'localhost',
-//     port: server.server.address().port,
-//     path: '/export',
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json',
-//       'Content-Length': Buffer.byteLength(mockBody),
-//     },
-//   };
+  if (isHttpResponse(response)) {
+    assert.strictEqual(response.status, 200, 'Expected status code 200');
+    assert.match(response.body, /html/, 'Expected response to include "html"');
+  } else {
+    throw new Error('Bad response');
+  }
+});
 
-//   const response = await new Promise((resolve, reject) => {
-//     const req = http.request(options, (res) => {
-//       let data = '';
-//       res.on('data', (chunk) => (data += chunk));
-//       res.on('end', () => resolve({ status: res.statusCode, body: JSON.parse(data) }));
-//     });
-//     req.on('error', reject);
-//     req.write(mockBody);
-//     req.end();
-//   });
+test('should handle POST /export successfully', async () => {
+  const address = server.server.address() as AddressInfo;
+  const mockBody = JSON.stringify([
+    {
+      store: 'testStore',
+      set: 'testSet',
+      schema: { keys: { SOME_KEY: 'string' } },
+      configs: { SOME_KEY: 'some value' },
+    },
+  ]);
 
-//   assert.strictEqual(response.status, 200, 'Expected status code 200');
-//   assert.strictEqual(typeof response.body, 'object', 'Expected response body to be an object');
-// });
+  const options = {
+    hostname: 'localhost',
+    port: address.port,
+    path: '/export',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': Buffer.byteLength(mockBody),
+    },
+  };
 
-// test('should return SSE headers if we pass a valid cron param', async () => {
-//   const mockBody = JSON.stringify([
-//     {
-//       store: 'testStore',
-//       set: 'testSet',
-//       schema: { keys: { SOME_KEY: 'string' } },
-//       configs: { SOME_KEY: 'some value' },
-//     },
-//   ]);
+  const response = await new Promise((resolve, reject) => {
+    const req = http.request(options, (res) => {
+      let data = '';
+      res.on('data', (chunk) => {
+        data += chunk;
+      });
+      res.on('end', () => resolve({ status: res.statusCode, body: JSON.parse(data) }));
+    });
+    req.on('error', reject);
+    req.write(mockBody);
+    req.end();
+  });
+  if (isHttpResponse(response)) {
+    assert.strictEqual(response.status, 200, 'Expected status code 200');
+    assert.strictEqual(typeof response.body, 'object', 'Expected response body to be an object');
+  } else {
+    throw new Error('Bad response');
+  }
+});
 
-//   const options = {
-//     hostname: 'localhost',
-//     port: server.server.address().port,
-//     path: '/export?cron=* * * * *',
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json',
-//       'Content-Length': Buffer.byteLength(mockBody),
-//     },
-//   };
+test('should return SSE headers if we pass a valid cron param', async () => {
+  const address = server.server.address() as AddressInfo;
+  const mockBody = JSON.stringify([
+    {
+      store: 'testStore',
+      set: 'testSet',
+      schema: { keys: { SOME_KEY: 'string' } },
+      configs: { SOME_KEY: 'some value' },
+    },
+  ]);
 
-//   const response = await new Promise((resolve, reject) => {
-//     const req = http.request(options, (res) => {
-//       let data = '';
-//       res.on('data', (chunk) => (data += chunk));
-//       res.on('end', () => resolve({
-//         status: res.statusCode,
-//         headers: res.headers,
-//         body: data,
-//       }));
-//     });
-//     req.on('error', reject);
-//     req.write(mockBody);
-//     req.end();
-//   });
+  const options = {
+    hostname: 'localhost',
+    port: address.port,
+    path: '/export?cron=* * * * *',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': Buffer.byteLength(mockBody),
+    },
+  };
 
-//   assert(response.headers['content-type'], 'Content-Type header is missing');
-//   assert.match(
-//     response.headers['content-type'],
-//     /text\/event-stream/i,
-//     'Expected content-type to be text/event-stream'
-//   );
-//   assert.strictEqual(response.status, 200, 'Expected status code 200');
-// });
+  const response = await new Promise((resolve, reject) => {
+    const req = http.request(options, (res) => {
+      let data = '';
+      res.on('data', (chunk) => {
+        data += chunk;
+      });
+      res.on('end', () =>
+        resolve({
+          status: res.statusCode,
+          headers: res.headers,
+          body: data,
+        }),
+      );
+    });
+    req.on('error', reject);
+    req.write(mockBody);
+    req.end();
+  });
+
+  if (isHttpResponse(response)) {
+    assert(response.headers['content-type'], 'Content-Type header is missing');
+    assert.match(
+      response.headers['content-type'],
+      /text\/event-stream/i,
+      'Expected content-type to be text/event-stream',
+    );
+    assert.strictEqual(response.status, 200, 'Expected status code 200');
+  } else {
+    throw new Error('Bad response');
+  }
+});
