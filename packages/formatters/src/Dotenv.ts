33b5ebc796d5790d9fcf9babd24651044a27a795
Or Levi <orlevi128@gmail.com>
2025-03-06 00:22:48 +0200

    fix(formatters): export dotenv with special characters (#717)
    * fix(formatters): export dotenv with special charachters
    
    * format fix
    
    * Revert "format fix"
    
    This reverts commit 16ba0db79aa3ca68641ec1852329e1d9a3c0ddd4.
    
    ---------
    
    Co-authored-by: Ran Cohen <rannn505@outlook.com>
    Co-authored-by: Ran Cohen <ran@configu.com>

--- packages/formatters/src/Dotenv.ts
+++ packages/formatters/src/Dotenv.ts
@@ -1,11 +1,15 @@
 import { type FormatterFunction } from './ConfigFormatter';
 import { hasWhitespace } from './utils';
 
+const hasSpecialCharacters = (value: string) => {
+  return hasWhitespace(value) || /[#^]/.test(value);
+};
+
 export const DotenvFormatter: FormatterFunction = (configs, { wrap = false }: { wrap?: boolean } = {}) => {
   return Object.entries(configs)
     .map(([key, value]) => {
-      if (wrap || (typeof value === 'string' && hasWhitespace(value))) {
-        return `${key}="${value}"`; // * in case value has a whitespace or wrap is true, wrap with quotes around it
+      if (wrap || (typeof value === 'string' && hasSpecialCharacters(value))) {
+        return `${key}="${value}"`; // * in case value has a special charachter or wrap is true, wrap with quotes around it
       }
       return `${key}=${value}`;
     })
